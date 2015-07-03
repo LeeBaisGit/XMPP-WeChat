6704c526319ccb189402c366d06c5d1d7c7129b7
李白 <libai@libaideMac.local>
2015-07-03 13:05:27 +0800

    Initial Commit

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by 李白 on 15-7-3.
+//  Copyright (c) 2015年 李白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by 李白 on 15-7-3.
+//  Copyright (c) 2015年 李白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
